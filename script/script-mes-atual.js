6dea5b2ecbdee9f731251621d96f469d6e4465f3
Richard Oliveira <richard.097@hotmail.com>
2022-10-10 17:18:01 -0300

    dia2

---
+++ script/script-mes-atual.js
@@ -0,0 +1,10 @@
+let desc = document.getElementById('descricao')
+let data = document.getElementById('data')
+let cat = document.getElementById('categoria')
+let val = document.getElementById('valor')
+let valor = Number(val.value)
+let lancamentos = []
+
+function lancar() {
+
+}
