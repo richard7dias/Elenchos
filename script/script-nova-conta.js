b84a8d0df35466bfe0e051f88d6bb4fb3532b071
Richard Oliveira <richard.097@hotmail.com>
2022-10-11 21:28:50 -0300

    cadastro

---
+++ script/script-nova-conta.js
@@ -0,0 +1,10 @@
+let nome = document.getElementById('nome')
+let sobrenome = document.getElementById('sobrenome')
+let email = document.getElementById('email')
+let senha = document.getElementById('senha')
+let confsenha = document.getElementById('conf-senha')
+let res = document.getElementById('msg')
+
+function cadastro() {
+
+}
